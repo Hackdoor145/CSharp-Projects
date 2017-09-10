747982af3d0dfdffdc3cb81f15e2204f50fe49af
Hackdoor145 <miguel.alex.salgueiro@outlook.pt>
2017-09-10 18:02:19 +0100

    Finished program
    Finished the program added motorway corrected bugs

--- Motorway/Doyle_04_02_Motorway/Motorway/Motorway.cs
+++ Motorway/Doyle_04_02_Motorway/Motorway/Motorway.cs
@@ -8,13 +8,29 @@ namespace Doyle_04_02_Motorway
 {
     class Motorway
     {
+        public enum IsToll
+        {
+            Yes,
+            No,
+            Na
+        }
+
+        public enum Directions
+        {
+            North,
+            East,
+            South,
+            West,
+            Na
+        }
+
         public string Name { get; set; }
-        public string MotorwayType { get; set; }
+        public string MotorwayType { get; set; } = "Na";
         public char Direction { get; set; }
-        public string Surface { get; set; }
-        public int NumberOfLanes { get; set; }
-        public bool Toll { get; set; }
-        public string Maintenance { get; set; }
+        public string Surface { get; set; } = "Na";
+        public int? NumberOfLanes { get; set; }
+        public bool? Toll { get; set; }
+        public string Maintenance { get; set; } = "Na";
 
         public Motorway(string name, string type, char direction, string surface, int numOfLanes, bool toll, string maint)
         {
@@ -44,14 +60,53 @@ namespace Doyle_04_02_Motorway
             NumberOfLanes = numOfLanes;
         }
 
+        private IsToll IsMotorwayToll()
+        {
+            switch (Toll)
+            {
+                case true:
+                    return IsToll.Yes;
+                case false:
+                    return IsToll.No;
+                default:
+                    return IsToll.Na;
+
+            }
+        }
+
+        private Directions GetDirections()
+        {
+            switch(Direction)
+            {
+                case 'N':
+                    return Directions.North;
+                case 'E':
+                    return Directions.East;
+                case 'S':
+                    return Directions.South;
+                case 'W':
+                    return Directions.West;
+                default:
+                    return Directions.Na;
+            }
+        }
+
+        private string HowManyLanes()
+        {
+            if (NumberOfLanes == null)
+                return "Na";
+            else
+                return NumberOfLanes.ToString();
+        }
+
         public override string ToString()
         {
             return $"Motorway Name: {Name}\n" +
                     $"Motorway Type: {MotorwayType}\n" +
-                    $"Direction: {Direction}\n" +
+                    $"Direction: {GetDirections()}\n" +
                     $"Surface: {Surface}\n" +
-                    $"Number Of Lanes: {NumberOfLanes}\n" +
-                    $"Toll: {Toll}\n" +
+                    $"Number Of Lanes: {HowManyLanes()}\n" +
+                    $"Toll: {IsMotorwayToll()}\n" +
                     $"Maintenance: {Maintenance}";
         }
     }
--- Motorway/Doyle_04_02_Motorway/Motorway/Program.cs
+++ Motorway/Doyle_04_02_Motorway/Motorway/Program.cs
@@ -12,7 +12,18 @@ namespace Doyle_04_02_Motorway
         {
             var motorway1 = new Motorway("Motorway1", "Highway", 'S', "Asphalt", 3, false, "Maintenance Company");
             Console.WriteLine(motorway1+"\n");
-            
+
+            var motorway2 = new Motorway("Motorway2", true);
+            Console.WriteLine(motorway2 + "\n");
+
+            var motorway3 = new Motorway("Motorway3");
+            Console.WriteLine(motorway3 + "\n");
+
+            var motorway4 = new Motorway("Motorway4", 2);
+            Console.WriteLine(motorway4 + "\n");
+
+            Console.ReadLine();
+
         }
     }
 }
